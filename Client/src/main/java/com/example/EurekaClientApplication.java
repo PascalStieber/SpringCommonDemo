2771894c911647d87261a433a0091a172dc5d436
pasca <pasca@DESKTOP-QNCN2TH>
2017-08-08 22:04:12 +0200

    :-)

--- Client/src/main/java/com/example/EurekaClientApplication.java
+++ Client/src/main/java/com/example/EurekaClientApplication.java
@@ -1,6 +1,11 @@
 package com.example;
 
+import java.io.IOException;
+
 import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -13,13 +18,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.web.context.request.RequestContextListener;
 
 @EnableOAuth2Sso
@@ -33,7 +38,7 @@ public class EurekaClientApplication extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 	
-		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**").permitAll().anyRequest()
+		http.antMatcher("/**").authorizeRequests().antMatchers("/").permitAll().anyRequest()
 				.authenticated();
 
 		http.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
@@ -48,16 +53,23 @@ public class EurekaClientApplication extends WebSecurityConfigurerAdapter {
 	ResourceServerProperties resourceServerProperties;
 
 	private Filter ssoFilter() {
-//		if(oauth2ClientContext.getAccessToken()!=null)
-		OAuth2ClientAuthenticationProcessingFilter oAuuth2Filter = new OAuth2ClientAuthenticationProcessingFilter(
-				"/securedPage");
-		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(resourceDetails, OAuth2ClientContext);
-		oAuuth2Filter.setRestTemplate(facebookTemplate);
+//		OAuth2ClientAuthenticationProcessingFilter oAuuth2Filter = new OAuth2ClientAuthenticationProcessingFilter(
+//				"/securedPage");
+		SsoFilter oAuth2Filter = new SsoFilter("/securedPage");
+		OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails, OAuth2ClientContext);
+		oAuth2Filter.setRestTemplate(restTemplate);
 		UserInfoTokenServices tokenServices = new UserInfoTokenServices(resourceServerProperties.getUserInfoUri(),
 				resourceServerProperties.getClientId());
-		tokenServices.setRestTemplate(facebookTemplate);
-		oAuuth2Filter.setTokenServices(tokenServices);
-		return oAuuth2Filter;
+		tokenServices.setRestTemplate(restTemplate);
+		oAuth2Filter.setTokenServices(tokenServices);
+		
+		oAuth2Filter.setAuthenticationSuccessHandler(new SimpleUrlAuthenticationSuccessHandler() {
+		    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+		        this.setDefaultTargetUrl("http://192.168.99.101:9999/client/securedPage2");
+		        super.onAuthenticationSuccess(request, response, authentication);
+		    }
+		});
+		return oAuth2Filter;
 	}
 
 	@Bean
--- Zuul/src/main/java/com/example/demo/ZuulApplication.java
+++ Zuul/src/main/java/com/example/demo/ZuulApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -15,8 +14,4 @@ public class ZuulApplication {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 
-	@Bean
-	public ContentUrlRewritingFilter contentUrlRewritingFilter() {
-		return new ContentUrlRewritingFilter("http://172.18.0.5:8080/", "192.168.99.101:9999/client/securedPage2");
-	}
 }
