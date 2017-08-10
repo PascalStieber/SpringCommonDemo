4ac1f004a3bccbc355989773899fb08c76195b9b
pasca <pasca@DESKTOP-QNCN2TH>
2017-08-10 22:30:32 +0200

    added mongodb capabilities

--- Client/src/main/java/com/example/WebController.java
+++ Client/src/main/java/com/example/WebController.java
@@ -2,19 +2,11 @@ package com.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 //@Configuration
 //@EnableWebMvc
@@ -23,46 +15,75 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebController {
 
 	Logger log = LoggerFactory.getLogger(ClientApplication.class);
-	
-	@RequestMapping("/securedPage")
-	public ModelAndView securedPage(){
-		return new ModelAndView("securedPage");
-	}
-	
+
+	@Autowired
+	private CustomerRepository repository;
+
 	@Value("${example.property:default}")
 	private String exampleProperty;
-	
-	@RequestMapping("/securedPage2")
-	public ModelAndView securedPage2(){
+
+	@RequestMapping("/securedPage")
+	public ModelAndView securedPage() {
 		return new ModelAndView("securedPage");
 	}
-	
+
+	@RequestMapping("/securedPage2")
+	public ModelAndView securedPage2() {
+		return new ModelAndView("securedPage");
+	}
+
+	@RequestMapping("/customers")
+	public void getCustomers() {
+		repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+	}
+
 	@RequestMapping("/")
-	public ModelAndView index(){
-	    System.out.println(exampleProperty);
+	public ModelAndView index() {
+		System.out.println(exampleProperty);
 		return new ModelAndView("index");
 	}
 
-
-
-
-//	@Override
-//	public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
-//		configurer.enable();
-//	}
-//
-//	@Override
-//	public void addViewControllers(final ViewControllerRegistry registry) {
-//		super.addViewControllers(registry);
-//		registry.addViewController("/").setViewName("forward:/index");
-//		registry.addViewController("/index");
-//		registry.addViewController("/securedPage");
-//		registry.addViewController("/securedPage2");
-//	}
-//
-//	@Override
-//	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-//		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-//	}
+	// @Override
+	// public void configureDefaultServletHandling(final
+	// DefaultServletHandlerConfigurer configurer) {
+	// configurer.enable();
+	// }
+	//
+	// @Override
+	// public void addViewControllers(final ViewControllerRegistry registry) {
+	// super.addViewControllers(registry);
+	// registry.addViewController("/").setViewName("forward:/index");
+	// registry.addViewController("/index");
+	// registry.addViewController("/securedPage");
+	// registry.addViewController("/securedPage2");
+	// }
+	//
+	// @Override
+	// public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	// registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	// }
 
 }
