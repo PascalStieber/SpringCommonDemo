d5149ee806f3effc93ada431818d57e2964d2cd2
PST <PST@NLWE2151.opitz-consulting.int>
2017-08-29 16:29:10 +0200

    added boot admin as a service

---
+++ Admin/src/main/java/com/example/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
