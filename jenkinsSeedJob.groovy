63ad45c492dfa7637b36a721b7b7cb04fbe6d5b9
pascalstieber <pascalstieber@pascals-mbp.fritz.box>
2018-01-11 16:32:12 +0100

    added new release job

--- jenkinsSeedJob.groovy
+++ jenkinsSeedJob.groovy
@@ -1,6 +1,27 @@
 mavenJob('buildSpringCommonDemo'){
+	logRotator {
+        numToKeep(5)
+        artifactNumToKeep(1)
+    }
+    triggers{
+    		pollSCM{scmpoll_spec('H/5 * * * *')}
+    }
 	scm{
 		github('PascalStieber/SpringCommonDemo')
 	}
 	goals('clean install')
+}
+
+mavenJob('releaseSpringCommonDemo'){
+	logRotator {
+        numToKeep(5)
+        artifactNumToKeep(1)
+    }
+    //triggers{
+    	//	pollSCM{scmpoll_spec('H/30 * * * *')}
+    //}
+	scm{
+		github('PascalStieber/SpringCommonDemo')
+	}
+	goals('release:prepare release:perform')
 }
