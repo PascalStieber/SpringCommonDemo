c047c610b6ad0fdc31093366ccf8a7bca5ab34db
pascalstieber <pascalstieber@192.168.99.1>
2018-02-08 09:48:56 +0100

    initial devops_microservices setup #section one

--- client/src/main/java/com/example/ClientApplication.java
+++ client/src/main/java/com/example/ClientApplication.java
@@ -8,16 +8,17 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableOAuth2Sso
 @SpringBootApplication
-@EnableDiscoveryClient
-@RefreshScope
-@EnableHystrix
-@EnableCircuitBreaker
+@RestController
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String hallowelt() {
+		return "Hallo Welt";
 	}
 
 }
--- startDockerDeamons.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube 
-#docker run -d --name jenkins -p 8080:8080 -p 50000:50000 -v ~/docker_data/jenkins_home:/var/jenkins_home jenkins/jenkins:lts 
-#docker run -d --name mongoclient -p 3000:3000 mongoclient/mongoclient 
-#docker run -d --name nexus -p 8081:8081  sonatype/nexus 
