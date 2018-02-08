ec2947e46be216d226049ae026cee4abb8f490e0
pascalstieber <pascalstieber@192.168.99.1>
2018-02-08 09:50:43 +0100

    bugfix

--- client/src/main/java/com/example/ClientApplication.java
+++ client/src/main/java/com/example/ClientApplication.java
@@ -2,11 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
