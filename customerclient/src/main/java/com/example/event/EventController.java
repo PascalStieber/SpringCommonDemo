e56e77896678e61517b49bf6c9405e29900a83c4
pascalstieber <pascalstieber@pascals-mbp.fritz.box>
2018-07-21 09:15:08 +0200

    made kafka event producing / consuming more convinient

--- customerclient/src/main/java/com/example/event/EventController.java
+++ customerclient/src/main/java/com/example/event/EventController.java
@@ -1,19 +1,52 @@
 package com.example.event;
 
+import java.util.HashMap;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventController {
-	
+
+	public static Logger logger = LoggerFactory.getLogger(EventController.class);
+	// private final CountDownLatch latch = new CountDownLatch(3);
+
 	@Autowired
-	SimpleSourceBean simpleSourceBean;
+	private KafkaTemplate<String, String> template;
 
 	@RequestMapping("/publishEvent")
-	public ModelAndView publishEvent() {
-		simpleSourceBean.publishEvent();
-		return new ModelAndView("index");
+	public String publishEvent() {
+		this.template.send("eventtopic", "foo1");
+		this.template.send("eventtopic", "foo2");
+		this.template.send("eventtopic", "foo3");
+		// latch.await(60, TimeUnit.SECONDS);
+		return "Messaging proceeded...";
 	}
+
+	@KafkaListener(topics = "eventtopic")
+	public void listen(String pMessage) {
+		System.out.println("Listener received: " + pMessage);
+	}
+
+
+	// @Autowired
+	// SimpleSourceBean simpleSourceBean;
+	//
+	// @RequestMapping("/publishEvent")
+	// public ModelAndView publishEvent() {
+	// simpleSourceBean.publishEvent();
+	// return new ModelAndView("index");
+	// }
 }
